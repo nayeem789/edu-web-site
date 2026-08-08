1033677454ffa0ff94a3563d57668319caffb141
nayeem789 <opnayem12388@gmail.com>
2026-08-08 20:41:27 +0600

    scroll update

--- script.js
+++ script.js
@@ -382,3 +382,18 @@ function handleFormSubmit(e) {
        console.log("Error:", error);
     });
 }
+
+let lastScrollTop = 0;
+const header = document.querySelector('header');
+
+window.addEventListener('scroll', function() {
+  let scrollTop = window.pageYOffset || document.documentElement.scrollTop;
+
+  if (scrollTop > lastScrollTop && scrollTop > 50) {
+    header.classList.add('header-hidden');
+  } else {
+    header.classList.remove('header-hidden');
+  }
+
+  lastScrollTop = scrollTop <= 0 ? 0 : scrollTop;
+});
